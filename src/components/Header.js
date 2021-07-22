8b02ee5641849a6d20e876f1c817372736a51029
Billy Kneuper <wkneuper@gmail.com>
2021-07-22 13:20:27 -0700

    Added NavBar, logo and draft slogan

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+import robot from '../images/Robot.png';
+
+import './Header.css';
+
+const HeaderLink = ({ page }) => {
+  const title = page.charAt(0).toUpperCase() + page.slice(1);
+
+  return <Link className="headerlink-title" to={`/${page}`}>{title}</Link>;
+};
+
+const Header = () => {
+    return (
+        <>
+      <div className='header'>
+        <HeaderLink page='home' />
+        <HeaderLink page='voter' />
+        <HeaderLink page='ballots' />
+        <HeaderLink page='elections' />
+      </div>
+      <div><br />
+        <img src={robot} height={100} width={100} />
+        <h1 style={{color: "F57222"}}>Team React Rookies - Developing cutting edge React components since yesterday!</h1> 
+        <hr />
+      </div>
+      </>
+    );
+  };
+  
+  export default Header;
